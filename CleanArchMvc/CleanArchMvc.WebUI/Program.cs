b113148bd4ae2f54d110306812bcb06b397c3476
Pablo.Quadros <pablo.aquadros@gmail.com>
2023-02-22 11:19:48 -0300

    Recreating CleanAtchMvc.WebUI

--- CleanArchMvc/CleanArchMvc.WebUI/Program.cs
+++ CleanArchMvc/CleanArchMvc.WebUI/Program.cs
@@ -3,14 +3,14 @@ using CleanArchMvc.Infra.IoC;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 builder.Services.AddInfrastructure(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -22,6 +22,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Categories}/{action=Index}/{id?}");
 
 app.Run();
